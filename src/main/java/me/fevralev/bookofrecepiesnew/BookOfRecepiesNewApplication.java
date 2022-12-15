47ceb47e4535b7c3db3fe0579e153edfc3fde1ec
afevralevtotyt <afevraleff@ya.ru>
2022-12-15 22:17:35 +0200

    Initial commit

---
+++ src/main/java/me/fevralev/bookofrecepiesnew/BookOfRecepiesNewApplication.java
@@ -0,0 +1,13 @@
+package me.fevralev.bookofrecepiesnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOfRecepiesNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOfRecepiesNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/fevralev/bookofrecepiesnew/BookOfRecepiesNewApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fevralev.bookofrecepiesnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOfRecepiesNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
